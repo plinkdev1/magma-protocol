ab36d94b550ecc15ac23bec2f022e4bcc809970a
plinkdev1 <plink.dev1@gmail.com>
2026-03-10 03:56:38 +0000

    chore: switch API to production for APK build

---
+++ patch-prod.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+const fp = 'src/config.ts';
+fs.writeFileSync(fp, `const DEV = false;
+export const API_URL = DEV
+  ? 'http://10.0.2.2:3000'
+  : 'https://magma-backend-production-9f5b.up.railway.app';
+`);
+console.log('✅ API set to production');
